6dd5d3724d2be5493bbba0b8ee8b37107a8f5fc7
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-10-24 11:38:48 +0200

    saxon acabado

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -40,8 +40,19 @@ class Viking(Soldier):
 
 # Saxon
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        super().__init__(health, strength)
+    
+    def attack(self):
+        return super().attack()
+    
+    def receiveDamage(self, damage):
+        self.health=self.health-damage
+        if(self.health>0):
+            return f'A Saxon has received {damage} points of damage'
+        else:
+            return f'A Saxon has died in combat'
 
 # War
 
