122a6a0a5b37ea7a9cf9301e88fba88b9f369731
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-10-24 11:34:22 +0200

    vikingos acabados

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -19,12 +19,26 @@ class Soldier:
 
 # Viking
 
-
 class Viking(Soldier):
     
+    def __init__(self,name, health, strength):
+        super().__init__(health, strength)
+        self.name=name
+
+    def attack(self):
+        return super().attack()
+    
+    def receiveDamage(self, damage):
+        self.health=self.health-damage
+        if(self.health>0):
+            return f'{self.name} has received {damage} points of damage'
+        else:
+            return f'{self.name} has died in act of combat'
+
+    def battleCry(self):
+        return 'Odin Owns You All!'
 
 # Saxon
-
 
 class Saxon:
     pass
