5abc419d5d155d7a66274b60b21f1e9697b51ecb
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-10-24 12:04:28 +0200

    war acabado

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -56,6 +56,37 @@ class Saxon(Soldier):
 
 # War
 
-
 class War:
-    pass
+    def __init__(self):
+        self.vikingArmy=[]
+        self.saxonArmy=[]
+
+    def addViking(self,viking:Viking):
+        self.vikingArmy.append(viking)
+    
+    def addSaxon(self,saxon:Saxon):
+        self.saxonArmy.append(saxon)
+    
+    def vikingAttak(self):
+        r=Saxon.receiveDamage(Viking.strength)
+        if(Saxon.health>0):
+            return  f'{r}'
+        else:
+            self.saxonArmy.remove(Saxon)
+
+    def saxonAttak(self):
+        s=Viking.receiveDamage(Saxon.strength)
+        if(Viking.health>0):
+            return  f'{s}'
+        else:
+            self.vikingArmy.remove(Viking)
+
+    def showStatus(self):
+        if(len(self.vikingArmy)==0):
+            return "Saxons have fought for their lives and survive another day..."
+        
+        elif(len(self.saxonArmy)==0):
+            return f'Vikings have won the war of the century!'
+
+        else:
+            return "Vikings and Saxons are still in the thick of battle."
