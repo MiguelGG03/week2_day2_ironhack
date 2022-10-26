595d9179adc7d4c8220b7a2bac2aa21bb4250e24
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-10-26 12:15:04 +0200

    files actualizado pero no acabado

---
+++ files.py
@@ -0,0 +1,26 @@
+from vikingsClasses import *
+
+def createVikingsTeam():
+    for i in range(10):
+        War.addViking
+
+
+def createSaxonsTeam():
+    for i in range(10):
+        War.addSaxon
+
+
+def main():
+    createSaxonsTeam()
+    createVikingsTeam()
+    print(War.vikingArmy)
+
+
+
+
+
+
+
+
+if __name__=='__main__':
+    main()
