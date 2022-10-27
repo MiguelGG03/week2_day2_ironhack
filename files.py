a0a95fb7ca02e1aa5c1afa27f002514f950301f0
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-10-27 10:15:03 +0200

    bonus acabado

--- files.py
+++ files.py
@@ -1,20 +1,30 @@
 from vikingsClasses import *
-
-def createVikingsTeam():
-    for i in range(10):
-        War.addViking
-
-
-def createSaxonsTeam():
-    for i in range(10):
-        War.addSaxon
-
+from vikingsClasses import (Viking,Saxon,War,Soldier)
 
 def main():
-    createSaxonsTeam()
-    createVikingsTeam()
-    print(War.vikingArmy)
+    saxon=Saxon(100,98)
+    viking=Viking('Lewandoski',100,99)
+    guerra=War()
 
+    guerra.addSaxon(saxon)
+    guerra.addViking(viking)
+    ataque1=guerra.saxonAttack()
+    ataque2=guerra.vikingAttack()
+    estadisticas=guerra.showStatus()
+    lista1=guerra.saxonArmy
+    lista2=guerra.vikingArmy
+    print(f'Ataque Saxon: {ataque1}\n'
+          f'Ataque Viking: {ataque2}\n'
+          f'Estadisticas: {estadisticas}\n'
+          f'Ejercito Saxon: {lista1}\n'
+          f'Ejercito Viking: {lista2}\n')
+    #vuelvo a atacar para que uera un vikingo
+    ataque3=guerra.saxonAttack()
+    estadisticas2=guerra.showStatus()
+    print(f'Ataque Saxon: {ataque3}\n'
+          f'Estadisticas: {estadisticas2}\n'
+          f'Ejercito Saxon: {lista1}\n'
+          f'Ejercito Viking: {lista2}\n')
 
 
 
